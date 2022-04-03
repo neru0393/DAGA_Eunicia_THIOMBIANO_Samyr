68ebd987e15023ba79e6232a38a2503ee9952429
ccbobs13 <96637804+ccbobs13@users.noreply.github.com>
2022-04-03 22:44:18 +0000

    Update INSTALL_MYSQL.sh

--- INSTALL_MYSQL.sh
+++ INSTALL_MYSQL.sh
@@ -8,22 +8,22 @@ sudo apt-get update -y;
 sudo apt-get upgrade -y;
 
 #Installation du serveur
-#sudo apt-get install mysql-server -y;
+sudo apt-get install mysql-server -y;
 
 #Installation du client
-#sudo apt-get install mysql-client -y;
+sudo apt-get install mysql-client -y;
 
 #Affichage de la version
-#mysql --version;
+mysql --version;
 
 #Redemarrage du service
-#sudo systemctl restart mysql;
+sudo systemctl restart mysql;
 
 #Affichage du statut du service
-#sudo systemctl status mysql
+sudo systemctl status mysql
 
 #Création de la base de données 
 mysql -e 'CREATE DATABASE projet_reseau';
 
 #Création de la table
-Mysql -e 'USE projet_reseau;CREATE TABLE user(identifiant int(5),nom varchar(20),prenom varchar(20), password(20), PRIMARY KEY(identifiant))';
+mysql -e 'USE projet_reseau;CREATE TABLE user(identifiant int(5),nom varchar(20),prenom varchar(20), password(20), PRIMARY KEY(identifiant))';
